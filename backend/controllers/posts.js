d22cfe9403c244271541868797e28042d0910e61
edougher <ejdougher@gmail.com>
2021-05-17 23:48:25 -0400

    added controller and models files

---
+++ backend/controllers/posts.js
@@ -0,0 +1,10 @@
+import PostMessage from '../controllers/postMessage.js'
+
+export const getPosts = (req, res) => {
+    res.send("This works!")
+};
+
+export const createPost = (req, res) => {
+    res.send('Post Createt!')
+
+}
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+
+import postRoutes from './routes/posts.js'
 /*
 aaronwakeup
 Helloworld@sd86  
 */
 const app = express()
+
+app.use('/posts', postRoutes)
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
