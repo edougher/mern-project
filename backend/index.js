e8501e6fc45a8deb06358d1c2e94df58a5137a23
edougher <ejdougher@gmail.com>
2021-06-02 23:11:24 -0500

    created dotenv variables

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
+dotenv.config()
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://aaronwakeup:Helloworld@sd86@cluster0.rbpc1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//const CONNECTION_URL = 'mongodb+srv://aaronwakeup:Helloworld@sd86@cluster0.rbpc1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose.connect(`${process.env.CONNECTION_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
