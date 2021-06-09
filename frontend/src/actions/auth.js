c68971eb0b13253ae5cb47b7cb33989bcef5ce86
edougher <ejdougher@gmail.com>
2021-06-09 16:47:47 -0500

    fixed and created copy to deploy on netlify and heroku

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -6,7 +6,6 @@ export const signin = (formData, history) => async (dispatch) => {
         const { data } = await api.signIn(formData)
 
         dispatch({ type: AUTH, data })
-
         history.push('/')
     } catch (error) {
         console.log(error);
